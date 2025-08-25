3d2e90f4c21bf57a2c7b1cce96019ab052855c36
arkansoap <arkansoap@gmail.com>
2025-08-25 16:03:45 +0200

    update manual_deploy + ajout model_cron.txt

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -1,22 +1,29 @@
-# Serveur perso
+#!/bin/bash
+
 PROD_USER="root"
 SSH_HOST="5.182.18.114"
 PROD_FOLDER="/home/news_radar"
 
-
-
 echo "Starting Manual Deploy"
 sleep 1
 
+# Suppression du dossier local dist s'il existe
 rm -rf dist
+
+# Clonage du dépôt
 git clone git@github.com:arkansoap/news_radar.git dist/
 rm -rf dist/.git
+
+# Copie du fichier .env.prod
 cp -f .env.prod dist/.env || exit
 
-# Synchronize the new files
+# Suppression du dossier distant news_radar
+ssh $PROD_USER@$SSH_HOST "rm -rf $PROD_FOLDER"
+
+# Synchronisation des nouveaux fichiers
 rsync -av --progress dist/ $PROD_USER@$SSH_HOST:$PROD_FOLDER
 
-# Set up the environment
+# Configuration de l'environnement
 ssh $PROD_USER@$SSH_HOST << EOF
     cd $PROD_FOLDER
     python3.11 -m venv venv
